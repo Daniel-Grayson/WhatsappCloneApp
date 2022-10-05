5f3642da16389906d99424a40f8ae9cf62ace12d
Daniel Grayson <graysoncodes@gmail.com>
2022-10-05 22:56:33 +0100

    updated home screen

--- lib/info.dart
+++ lib/info.dart
@@ -2,70 +2,70 @@ const info = [
   {
     'name': 'Blossom',
     'message': 'Hey, how are you doing?',
-    'time': '3:53 pm',
+    'time': '3:53',
     'profilePic':
         'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
   },
   {
     'name': 'Grace',
     'message': 'Hello, whats up',
-    'time': '2:25 pm',
+    'time': '2:25',
     'profilePic':
         'https://www.socialketchup.in/wp-content/uploads/2020/05/fi-vill-JOHN-DOE.jpg',
   },
   {
     'name': 'Destiny',
     'message': 'Boss, afar?',
-    'time': '1:03 pm',
+    'time': '1:03',
     'profilePic':
         'https://media.cntraveler.com/photos/60596b398f4452dac88c59f8/16:9/w_3999,h_2249,c_limit/MtFuji-GettyImages-959111140.jpg',
   },
   {
     'name': 'Sir',
     'message': 'Call me, have some work',
-    'time': '12:06 pm',
+    'time': '12:06',
     'profilePic':
         'https://pbs.twimg.com/profile_images/1419974913260232732/Cy_CUavB.jpg',
   },
   {
     'name': 'Ma',
     'message': 'You ate food?',
-    'time': '10:00 am',
+    'time': '10:00',
     'profilePic':
         'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0',
   },
   {
     'name': 'Wisdom, G3',
     'message': 'Yo!!!!! Long time, no see!?',
-    'time': '9:53 am',
+    'time': '9:53',
     'profilePic':
         'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
   },
   {
     'name': 'Vanessa, Sis',
     'message': 'Am I fat?',
-    'time': '7:25 am',
+    'time': '7:25',
     'profilePic':
         'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
   },
   {
     'name': 'Graham, G3',
     'message': 'I am from International Olym...',
-    'time': '6:02 am',
+    'time': '6:02',
     'profilePic':
         'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
   },
   {
     'name': 'Wilson, Tutor',
     'message': 'Lets Code!',
-    'time': '4:56 am',
+    'time': '4:56',
     'profilePic':
         'https://images.unsplash.com/photo-1619194617062-5a61b9c6a049?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHJhbmRvbSUyMHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
   },
   {
     'name': 'Rachael lane',
     'message': 'Images by Unsplash',
-    'time': '1:00 am',
+    'time': '1:00',
     'profilePic':
         'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
   },
--- lib/widgets/contact_list.dart
+++ lib/widgets/contact_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone_app/colors.dart';
 import '../info.dart';
 
 class ContactsList extends StatelessWidget {
@@ -8,25 +9,47 @@ class ContactsList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.only(top: 10),
-        child: ListView.builder(
-            itemCount: info.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(
-                  info[index]["name"].toString(),
-                  style: const TextStyle(fontSize: 18),
-                ),
-                subtitle: Padding(
-                  padding: const EdgeInsets.only(top: 6.0),
-                  child: Text(info[index]["message"].toString(),
-                      style: const TextStyle(fontSize: 15)),
-                ),
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                    info[index]["profilePic"].toString(),
-                  ),
-                ),
-              );
-            }));
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: info.length,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: ListTile(
+                          title: Text(
+                            info[index]["name"].toString(),
+                            style: const TextStyle(fontSize: 18),
+                          ),
+                          subtitle: Padding(
+                            padding: const EdgeInsets.only(top: 2.0),
+                            child: Text(info[index]["message"].toString(),
+                                style: const TextStyle(fontSize: 15)),
+                          ),
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                              info[index]["profilePic"].toString(),
+                            ),
+                          ),
+                          trailing: Text(
+                            info[index]["time"].toString(),
+                            style: const TextStyle(
+                                fontSize: 13, color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+            const Divider(
+              color: dividerColor,
+              indent: 85,
+            )
+          ],
+        ));
   }
 }
