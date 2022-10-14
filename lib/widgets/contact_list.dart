aaaf94d9b960a603dfbca55c757edcdf44b88858
Daniel Grayson <graysoncodes@gmail.com>
2022-10-14 11:41:15 +0100

    updated layout

--- lib/widgets/contact_list.dart
+++ lib/widgets/contact_list.dart
@@ -7,49 +7,47 @@ class ContactsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.builder(
-          shrinkWrap: true,
-          itemCount: info.length,
-          itemBuilder: (context, index) {
-            return Column(
-              children: [
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const MobileChatScreen()),
-                    );
-                  },
-                  child: ListTile(
-                    title: Text(
-                      info[index]["name"].toString(),
-                      style: const TextStyle(fontSize: 18),
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: info.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const MobileChatScreen()),
+                  );
+                },
+                child: ListTile(
+                  title: Text(
+                    info[index]["name"].toString(),
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.only(top: 2.0),
+                    child: Text(info[index]["message"].toString(),
+                        style: const TextStyle(fontSize: 15)),
+                  ),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      info[index]["profilePic"].toString(),
                     ),
-                    subtitle: Padding(
-                      padding: const EdgeInsets.only(top: 2.0),
-                      child: Text(info[index]["message"].toString(),
-                          style: const TextStyle(fontSize: 15)),
-                    ),
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                        info[index]["profilePic"].toString(),
-                      ),
-                      radius: 30,
-                    ),
-                    trailing: Text(
-                      info[index]["time"].toString(),
-                      style: const TextStyle(
-                        fontSize: 13,
-                        color: Colors.grey,
-                      ),
+                    radius: 30,
+                  ),
+                  trailing: Text(
+                    info[index]["time"].toString(),
+                    style: const TextStyle(
+                      fontSize: 13,
+                      color: Colors.grey,
                     ),
                   ),
                 ),
-              ],
-            );
-          }),
-    );
+              ),
+            ],
+          );
+        });
   }
 }
